6ad57c03c369f9b48dcbd62f0ffebbd57848b186
TGSR <81268811+TGSR-code@users.noreply.github.com>
2023-05-29 22:52:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const Client = new Discord.Client();
-const token = 'ODcyNTI4MDcxNjMzMjg5MjU2.YQrLFw.7LplqQynf57pAq9fFcPiCC62ubs';
+const token = 'adonehere';
 
 const Prefix = '!'
 
